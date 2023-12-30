8378b40a2e46a45f66edb0baf15063285123ea86
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-30 09:36:21 +0100

    Connect Notes app backend to MongoDB database

---
+++ mongo.js
@@ -0,0 +1,38 @@
+// NPM Packages
+const mongoose = require('mongoose');
+
+if (process.argv.length > 3) {
+  console.log('Give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Dakouri:${password}@cluster0.racdybo.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+/* const note = new Note({
+  content: 'TypeScript is easy',
+  important: false,
+}); */
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log('note:', note);
+  });
+  mongoose.connection.close();
+});
+
+/* note.save().then((result) => {
+  console.log('Note saved successfully:');
+  mongoose.connection.close();
+}); */
