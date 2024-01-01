34f6c09e246784733853610072d852d3ef44a8bd
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-01 15:58:43 +0100

    Clean up code

--- index.js
+++ index.js
@@ -8,29 +8,7 @@ const Note = require('./models/note');
 
 const app = express();
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
-
-function generateId() {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
-  return maxId + 1;
-}
+let notes = [];
 
 function requestLogger(request, response, next) {
   console.log('Method:', request.method);
@@ -57,10 +35,6 @@ app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(requestLogger);
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello, World!</h1>');
-});
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
@@ -100,7 +74,6 @@ app.post('/api/notes', (request, response) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
   });
 
   note.save().then((savedNote) => {
